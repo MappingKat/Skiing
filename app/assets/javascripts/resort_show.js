9d8dd61930a2a59f4e68bbc2ee5214f5c5b59374
paulvarga23 <paul_varga@me.com>
2014-06-15 23:18:15 +0100

    weather forecast css done

---
+++ app/assets/javascripts/resort_show.js
@@ -0,0 +1,82 @@
+$(function() { 
+
+  var $nav = $("nav");
+  var $nav_links = $("nav a");
+
+  var $nav_one = $("#nav_one");
+  var $nav_two = $("#nav_two");
+  var $nav_three = $("#nav_three");
+
+  var $section_one = $("#one"); // overview
+  var $section_two = $("#two"); // reviews
+  var $section_three = $("#three"); // weather
+
+    $nav_links.click( function(event) {
+    $.scrollTo(
+      $(this).attr("href"),
+      {
+        duration: 2000,
+        offset: { 'left':0, 'top':500 }
+      }
+    );
+  });
+
+  // Updates menu styling when scrolling through the associated element.
+
+  function removeActive() {
+    $nav_links.removeClass('active');
+  };
+
+  // sky section
+  $section_one.waypoint({
+    handler: function(direction){
+      if (direction == "down") {
+        removeActive();
+        $nav_one.addClass('active')
+      }
+    },
+    // offset: -50
+  });
+
+  $section_one.waypoint({
+    handler: function(direction){
+      if (direction == "up") {
+        removeActive();
+        $nav_one.addClass('active')
+      }
+    },
+    offset: function() {
+      return -$(this).height();
+    }
+  });
+
+  $section_two.waypoint({
+    handler: function(direction){
+      if (direction == "down") {
+        removeActive();
+        $nav_two.addClass('active')
+      }
+    },
+  });
+
+  $section_two.waypoint({
+    handler: function(direction){
+      if (direction == "up") {
+        removeActive();
+        $nav_two.addClass('active')
+      }
+    },
+    offset: function() {
+      return -$(this).height();
+    }
+  });
+
+  $section_three.waypoint({
+    handler: function(direction){
+      if (direction == "down") {
+        removeActive();
+        $nav_three.addClass('active')
+      }
+    }
+  });
+});
