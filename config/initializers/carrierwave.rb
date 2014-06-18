c84c01d5ee4b2ab4a6dcc21aae60c22c35086c88
paulvarga23 <paul_varga@me.com>
2014-06-18 12:49:31 +0100

    update carrierwave to work both locally and in production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'wdi5-project3-weare'                    # required
-  config.fog_public     = true                               # optional, defaults to true
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],        # required
+      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'wdi5-project3-weare'                    # required
+    config.fog_public     = true                               # optional, defaults to true
+  end
 end
